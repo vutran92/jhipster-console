e432643fecd2987763744e25858b71eafb3d5313
Pierre Besson <pbesson@ippon.fr>
2017-12-15 18:04:24 +0100

    migrate dashboards to new 6.X format

--- jhipster-import-dashboards/create-index-patterns.sh
+++ jhipster-import-dashboards/create-index-patterns.sh
@@ -3,3 +3,5 @@
 echo "Creating index patterns if not present"
 curl -XPUT $ELASTICSEARCH_URL/.kibana/index-pattern/logstash-* -d '{"title" : "logstash-*",  "timeFieldName": "@timestamp"}'
 curl -XPUT $ELASTICSEARCH_URL/.kibana/config/${ELASTIC_VERSION} -d '{"defaultIndex" : "logstash-*"}'
+
+curl -XPOST $KIBANA_URL/api/saved_objects/index-pattern -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
