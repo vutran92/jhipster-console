293606005db108f5ad41403450d4545cb4e42e18
Pierre Besson <pbesson@ippon.fr>
2017-12-22 14:53:18 +0100

    use the new 6.x method to import index patterns

--- jhipster-import-dashboards/create-index-patterns.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Create index patterns in Elasticsearch
-echo "Creating index patterns if not present"
-curl -XPUT $ELASTICSEARCH_URL/.kibana/index-pattern/logstash-* -d '{"title" : "logstash-*",  "timeFieldName": "@timestamp"}'
-curl -XPUT $ELASTICSEARCH_URL/.kibana/config/${ELASTIC_VERSION} -d '{"defaultIndex" : "logstash-*"}'
-
-curl -XPOST $KIBANA_URL/api/saved_objects/index-pattern -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
--- jhipster-import-dashboards/wait-for-elasticsearch.sh
+++ jhipster-import-dashboards/wait-for-elasticsearch.sh
@@ -3,7 +3,7 @@
 echo "Waiting for Elasticsearch to startup (max 5min)"
 WAIT=0
 while [ $WAIT -lt 300 ]; do
-    curl ${ELASTICSEARCH_URL}/_cluster/health 2>/dev/null && echo "Importing dashboards" && break
+    curl ${ELASTICSEARCH_URL}/_cluster/health 2>/dev/null && echo "" && echo "Importing dashboards" && break
     sleep 1
     (( WAIT++ ))
 done
