98c8a16157105db266b0e50006d0df8d57b10345
Pierre Besson <pbesson@ippon.fr>
2017-12-28 14:48:21 +0100

    update elastalert config

--- jhipster-alerter/start-elastalert.sh
+++ jhipster-alerter/start-elastalert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Based on https://github.com/krizsan/elastalert-docker
-echo "Waiting for Elasticsearch to startup"
+echo "Waiting for ES to startup"
 while true; do
-    curl ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} 2>/dev/null && break
+    curl ${ES_HOST}:${ES_PORT} 2>/dev/null && break
     sleep 1
 done
 echo "Starting Alerting"
@@ -16,25 +16,26 @@ else
 	echo "Container timezone not modified"
 fi
 
-if [[ -n "${ELASTICSEARCH_USERNAME:-}" ]]
+if [[ -n "${ES_USERNAME:-}" ]]
 then
-	flags="--user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}"
+	flags="--user ${ES_USERNAME}:${ES_PASSWORD}"
 else
 	flags=""
 fi
 
 cd /opt/elastalert
 
-if ! curl -f $flags ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} >/dev/null 2>&1
+# TODO do this in the import-dashboards container instead 
+if ! curl -f $flags ${ES_HOST}:${ES_PORT} >/dev/null 2>&1
 then
-	echo "Elasticsearch not available at ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}"
+	echo "ES not available at ${ES_HOST}:${ES_PORT}"
 else
-	if ! curl -f $flags ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status >/dev/null 2>&1
+	if ! curl -f $flags ${ES_HOST}:${ES_PORT}/elastalert_status >/dev/null 2>&1
 	then
-		echo "Creating Elastalert index in Elasticsearch..."
+		echo "Creating Elastalert index in ES..."
 	    elastalert-create-index --index elastalert_status --old-index ""
 	else
-	    echo "Elastalert index already exists in Elasticsearch."
+	    echo "Elastalert index already exists in ES."
 	fi
 fi
 
