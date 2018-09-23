6e018e342f0393ba8195a0c59a07aa1b3185fd7f
Pierre Besson <pbesson@ippon.fr>
2018-09-23 19:45:40 +0200

    update elastic stack to v6.4.1

--- jhipster-import-dashboards/wait-for-elasticsearch.sh
+++ jhipster-import-dashboards/wait-for-elasticsearch.sh
@@ -7,3 +7,6 @@ while [ $WAIT -lt 300 ]; do
     sleep 1
     (( WAIT++ ))
 done
+
+# set logstash-* as the default index pattern
+curl ${KIBANA_URL}/api/kibana/settings -H "Origin: ${KIBANA_URL}" -H 'kbn-version: 6.4.1' -H 'content-type: application/json' -H 'accept: application/json' -H "Referer: ${KIBANA_URL}/app/kibana" --data-binary '{"changes":{"defaultIndex":"logstash-*"}}'
