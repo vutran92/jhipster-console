966ff20c43fc972f26e40c823a9406fd677ebbe5
Pierre Besson <pbesson@ippon.fr>
2018-09-24 10:53:39 +0200

    add zipkin dashboard and index pattern

--- export-dashboards/export-dashboards.sh
+++ export-dashboards/export-dashboards.sh
@@ -6,4 +6,5 @@ go run export_dashboards.go -kibana $KIBANA_URL -dashboard b2c61940-e180-11e7-9c
 go run export_dashboards.go -kibana $KIBANA_URL -dashboard d712f650-e0eb-11e7-9c68-0b9a0f0c183c -output dashboards/logs-dashboard.json
 go run export_dashboards.go -kibana $KIBANA_URL -dashboard 607a1ab0-e0ef-11e7-9c68-0b9a0f0c183c -output dashboards/metrics-dashboard.json
 go run export_dashboards.go -kibana $KIBANA_URL -dashboard 4161cd50-9c77-11e8-bfa6-a7fdcbb4d022 -output dashboards/alerting-dashboard.json
+go run export_dashboards.go -kibana $KIBANA_URL -dashboard 81d34a00-bfd4-11e8-b5e5-cbdd0005bb7b -output dashboards/traces-dashboard.json
 
